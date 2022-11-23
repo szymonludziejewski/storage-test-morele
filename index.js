e043fe105003edcac25eda31661cf7b107334b71
Szymon Ludziejewski <szymon.ludziejewski@vizlib.com>
2022-11-23 15:37:57 +0100

    trial

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const database = require('./simple-database/database');
+
+const environment = process.env;
+const app = express();
+
+app.use(bodyParser.json())
+
+app.get('/', (_request, response) => {
+  response.json(database.readData('data'));
+});
+
+app.post('/', (request, response) => {
+  const id = (new Date()).getTime();
+  database.addData('data', id, request.body);
+  response.json({status: 'OK'});
+});
+
+app.listen(environment.PORT, () => {
+  console.log(`Server running on port ${environment.PORT}`);
+});
